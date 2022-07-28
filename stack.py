656c309835ac713c401160f2c712bb429361c3eb
Md Istiaq Ahmed <istiaqahmed121998@gmail.com>
2022-07-28 12:43:56 +0600

    infix_to_postfix added

--- stack.py
+++ stack.py
@@ -52,13 +52,66 @@ class ListStack:
         if self.is_empty():
             raise Error("size is empty")
         else:
-            data=self.top.data
-            self.top=self.top.next
+            data = self.top.data
+            self.top = self.top.next
+            return data
+
+    def peek(self):
+        if self.is_empty():
+            return -1
+        else:
+            data = self.top.data
             return data
 
 
-a = ListStack()
-a.push(1)
-a.push(12)
-a.push(13)
-print(a.pop())
+class Conversion:
+
+    def __init__(self):
+        self.stack = ListStack()
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
+
+    def notGreater(self, i):
+        try:
+            a = self.precedence[i]
+            b = self.precedence[self.stack.peek()]
+            return True if a <= b else False
+        except KeyError:
+            return False
+
+    def infix_to_postfix(self, exp):
+        """
+        If the character is an operand, add it to the output. If the character is a left parenthesis, push it onto the
+        stack. If the character is a right parenthesis, pop the stack and add each operator to the output until you see a
+        left parenthesis. Discard the pair of parentheses. If the character is an operator, *, /, +, or -, push it onto the
+        stack. However, first remove any operators already on the stack that have higher or equal precedence and add them to
+        the output
+
+        :param exp: The expression to be converted
+        :return: The output is the postfix expression.
+        """
+        output = ''
+        for i in exp:
+            if i.isalpha():
+                output += i
+
+            elif i == '(':
+                self.stack.push(i)
+
+            elif i == ')':
+                while self.stack.peek() != '(':
+                    a = self.stack.pop()
+                    output += a
+                self.stack.pop()
+
+            else:
+                while not self.stack.is_empty() and self.notGreater(i):
+                    output += self.stack.pop()
+                self.stack.push(i)
+        while not self.stack.is_empty():
+            output += self.stack.pop()
+
+        return output
+
+
+a = Conversion()
+print(a.infix_to_postfix('A+B-(C+D)+E'))
